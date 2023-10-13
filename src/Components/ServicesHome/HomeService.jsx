a90585c84b4ce16d2dd44f59887d8eca19b12e40
Abdirahman Abdulaziz <abdirinoahash@gmail.com>
2023-10-13 18:24:01 +0300

    skills sec start

--- src/Components/ServicesHome/HomeService.jsx
+++ src/Components/ServicesHome/HomeService.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Hservice.css";
+import Skills from "../Skills/Skills";
 
 const HService = () => {
   return (
@@ -43,6 +44,7 @@ const HService = () => {
           </div>
         </div>
       </div>
+      <Skills />
     </>
   );
 };
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './skills.css'
+import React from "react";
+import "./skills.css";
 
 const Skills = () => {
   return (
     <>
-        <div className="skills"></div>
+      <div className="skills">
+        <div className="skill-detail">
+          <h1>Elevating Front-End Development Skills.</h1>
+          <p>
+            Unlocking the potential of the digital realm through mastery of
+            Front-End Development.
+          </p>
+        </div>
+        <div className="skill-content"></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
